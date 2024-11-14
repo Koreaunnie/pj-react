6d9dcb6a588290b63261ac6b8efc434e2ba4c0f9
koreaunnie <hyunjung1995@gmail.com>
2024-11-14 16:41:01 +0900

    수정 버튼 클릭 시 수정 화면으로 이동

--- frontend/src/page/board/BoardView.jsx
+++ frontend/src/page/board/BoardView.jsx
@@ -87,6 +87,8 @@ export function BoardView() {
               </DialogFooter>
             </DialogContent>
           </DialogRoot>
+
+          <Button onClick={() => navigate(`/edit/${board.id}`)}>수정</Button>
         </Box>
       </Stack>
     </Box>
