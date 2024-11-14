bc09f9405d0cb3d6f350c8b2f19bfe945fb25944
koreaunnie <hyunjung1995@gmail.com>
2024-11-14 10:39:14 +0900

    게시판 > 글 작성 form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,102 @@
-import {createBrowserRouter, Outlet, RouterProvider, useNavigate} from "react-router-dom";
-import {Box, Flex, Stack} from "@chakra-ui/react";
-import React from "react";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import { Box, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import { Field } from "./components/ui/field.jsx";
+import { Button } from "./components/ui/button.jsx";
+import { useState } from "react";
+import axios from "axios";
 
 function Navbar() {
   const navigate = useNavigate();
 
-  return <Flex gap={3}>
-    <Box onClick={() => navigate("/")}>HOME</Box>
-    <Box onClick={() => navigate("/add")}>작성</Box>
-  </Flex>
+  return (
+    <Flex gap={3}>
+      <Box onClick={() => navigate("/")}>HOME</Box>
+      <Box onClick={() => navigate("/add")}>작성</Box>
+    </Flex>
+  );
 }
 
 function RootLayout() {
-  return <Stack>
-    <Box>
-      <Navbar/>
-    </Box>
-    <Box>
-      <Outlet/>
-    </Box>
-  </Stack>
-}
-
-function BoardAdd() {
-  return <Box>
-    <h3>게시물 작성</h3>
-  </Box>
+  return (
+    <Stack>
+      <Box>
+        <Navbar />
+      </Box>
+      <Box>
+        <Outlet />
+      </Box>
+    </Stack>
+  );
 }
 
 function BoardList() {
-  return <Box>
-    <h3>게시물 목록</h3>
-  </Box>
+  return (
+    <Box>
+      <h3>게시물 목록</h3>
+    </Box>
+  );
 }
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <RootLayout/>,
-  children: [
-    {
-      index: true,
-      element: <BoardList/>
-    },
-    {
-      path: "add",
-      element: <BoardAdd/>
-    }
-  ]
-}]);
+function BoardAdd() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
 
+  const handleSaveClick = () => {
+    axios.post("/api/board/add", {
+      title, // title: title 에서 축약됨
+      content,
+      writer,
+    });
+  };
+  return (
+    <Box>
+      <h3>게시물 작성</h3>
+      <Stack gap={5}>
+        <Field label={"제목"}>
+          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </Field>
+        <Field label={"본문"}>
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Field>
+        <Field label={"작성자"}>
+          <Input value={writer} onChange={(e) => setWriter(e.target.value)} />
+        </Field>
+        <Box>
+          <Button onClick={handleSaveClick}>저장</Button>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <BoardList />,
+      },
+      {
+        path: "add",
+        element: <BoardAdd />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
