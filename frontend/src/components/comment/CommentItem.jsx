944b677f1e97554a41fb50bdb510eaede52dcded
koreaunnie <hyunjung1995@gmail.com>
2024-11-20 11:37:00 +0900

    댓글 작성 후 commentList update 됨

--- frontend/src/components/comment/CommentItem.jsx
+++ frontend/src/components/comment/CommentItem.jsx
@@ -2,7 +2,7 @@ import { Box, Flex } from "@chakra-ui/react";
 
 export function CommentItem({ comment }) {
   return (
-    <Box boarder={"1px solid black"} m={5}>
+    <Box borderBottom={"1px solid black"} m={5}>
       <Flex justify={"space-between"}>
         <h3>{comment.memberId}</h3>
         <h4>{comment.inserted}</h4>
